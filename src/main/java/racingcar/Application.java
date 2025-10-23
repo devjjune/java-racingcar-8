a99e2260c6d55568d9d4e26d338892919d311bf1
황지윤 <hwangjiyoon@hwangjiyun-ui-noteubug.local>
2025-10-23 18:46:33 +0900

    feat: 사용자 입력을 처리하는 getName(), getTries() 메서드 추가
    - 자동차 이름과 시도 횟수를 입력받는 기능 구현
    - main()에서 입력 흐름 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+  public static void main(String[] args) {
+    List<String> participantList = getName();
+    int numberOfTries = getTries();
+  }
+
+  public static List<String> getName() {
+    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    String participantName = Console.readLine();
+    String[] participantArray = participantName.split(",");
+
+    return new ArrayList<>(List.of(participantArray));
+  }
+
+  public static int getTries() {
+    System.out.println("시도할 횟수는 몇 회인가요?");
+    String inputTries = Console.readLine();
+
+    return Integer.parseInt(inputTries);
+  }
 }
