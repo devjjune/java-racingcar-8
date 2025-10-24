bac443a4f2854e35a3b5d356cff0924f92bf720f
황지윤 <hwangjiyoon@hwangjiyun-ui-noteubug.local>
2025-10-24 14:16:22 +0900

    feat: 게임을 수행하는 메서드 추가
    - initializeCarScores(): 자동차 이름과 점수를 매핑
    - getScore(): 시도 횟수만큼 게임 진행
    - tryOneTime(): 각 자동차의 시도 수행
    - plusRandomNumber(): 랜덤 값에 따라 이동 결정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,22 +1,27 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
   public static void main(String[] args) {
-    List<String> participantList = getName();
+    List<String> carList = getName();
     int numberOfTries = getTries();
+    Map<String, Integer> scores = initializeCarScores(carList);
+    Map<String, Integer> result = getScore(numberOfTries, scores);
   }
 
   public static List<String> getName() {
     System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    String participantName = Console.readLine();
-    String[] participantArray = participantName.split(",");
+    String inputName = Console.readLine();
+    String[] carArray = inputName.split(",");
 
-    return new ArrayList<>(List.of(participantArray));
+    return new ArrayList<>(List.of(carArray));
   }
 
   public static int getTries() {
@@ -24,5 +29,37 @@ public class Application {
     String inputTries = Console.readLine();
 
     return Integer.parseInt(inputTries);
+  }
+
+  public static Map<String, Integer> initializeCarScores(List<String> carList) {
+    Map<String, Integer> scores = new HashMap<>();
+    for (int i = 0; i < carList.size(); i++) {
+      scores.put(carList.get(i), 0);
+    }
+    return scores;
+  }
+
+  public static Map<String, Integer> getScore(int numberOfTries, Map<String, Integer> scores) {
+    for (int i = 0; i < numberOfTries; i++) {
+      tryOneTime(scores);
+    }
+    return scores;
+  }
+
+  public static Map<String, Integer> tryOneTime(Map<String, Integer> scores) {
+    for (String eachCar : scores.keySet()) {
+      int eachScore = scores.get(eachCar);
+      eachScore = plusRandomNumber(eachScore);
+      scores.put(eachCar, eachScore);
+    }
+    return scores;
+  }
+
+  public static int plusRandomNumber(int eachScore) {
+    int randomNumber = Randoms.pickNumberInRange(0, 9);
+    if (randomNumber >= 4) {
+      eachScore += 1;
+    }
+    return eachScore;
   }
 }
