f46b0e95a7aaf6a048d0300480ff578014dfd6e0
황지윤 <hwangjiyoon@hwangjiyun-ui-noteubug.local>
2025-10-25 11:31:59 +0900

    refactor: 입력 처리 로직을 InputHandler 클래스로 분리

---
+++ src/main/java/racingcar/InputHandler.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputHandler {
+  private InputHandler() {}
+
+  public static List<String> getName() {
+    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    String inputName = Console.readLine();
+    String[] carArray = inputName.split(",");
+
+    return new ArrayList<>(List.of(carArray));
+  }
+
+  public static int getTries() {
+    System.out.println("시도할 횟수는 몇 회인가요?");
+    String inputTries = Console.readLine();
+
+    return Integer.parseInt(inputTries);
+  }
+}
