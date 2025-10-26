2386e852a28055678510a4d7cb90f573cf56b39b
황지윤 <hwangjiyoon@hwangjiyun-ui-noteubug.local>
2025-10-26 21:44:13 +0900

    test: 랜덤값에 따른 자동차 이동 결과 검증 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,30 +10,54 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
 
-    @Test
-    void 기능_테스트() {
-        assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
-        );
-    }
+  @Test
+  @DisplayName("랜덤값이 4와 3일 때, pobi는 전진하고 woni는 멈춘다.")
+  void carMovesOnlyWhenRandomOverFour() {
+    assertRandomNumberInRangeTest(
+        () -> {
+          run("pobi,woni", "1");
+          assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+        },
+        4,
+        3);
+  }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
+  @Test
+  @DisplayName("랜덤값이 6과 9일 때, pobi와 woni 모두 전진한다.")
+  void bothCarsMovesWhenRandomOverFour() {
+    assertRandomNumberInRangeTest(
+        () -> {
+          run("pobi,woni", "1");
+          assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+        },
+        6,
+        9);
+  }
+
+  @Test
+  @DisplayName("랜덤값이 1과 3일 때, pobi와 woni 모두 멈춘다.")
+  void bothCarsStopWHenRandomUnderFour() {
+    assertRandomNumberInRangeTest(
+        () -> {
+          run("pobi,woni", "1");
+          assertThat(output()).contains("pobi : ", "woni : ", "최종 우승자 : pobi, woni");
+        },
+        1,
+        3);
+  }
+
+  @Test
+  @DisplayName("5자 이상의 이름을 입력 시 예외가 발생한다.")
+  void throwsExceptionWhenNameExceedsLimit() {
+    assertSimpleTest(
+        () ->
             assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
+                .isInstanceOf(IllegalArgumentException.class));
+  }
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
+  @Override
+  public void runMain() {
+    Application.main(new String[] {});
+  }
 }
